b7700a585fcd0d76c9d1b7b706f30380e9d6d634
wangmeng <wangmeng@i72.com>
2024-04-02 13:27:59 +0800

    feat: update

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client"
 
 import { useEffect, useState } from "react"
@@ -75,7 +76,7 @@ export function Sidebar({ className, navItems }: SidebarProps) {
                       >
                         <div className="scale relative mb-2 flex items-center gap-2 rounded-r-lg p-2 transition-colors ease-in-out before:transition-colors hover:no-underline sm:border-l-0 sm:pl-6 sm:before:absolute sm:before:left-[-5px] sm:before:top-[2px] sm:before:h-[calc(100%-4px)] sm:before:w-[10px] sm:before:rounded-full sm:before:transition-colors">
                           <div className="relative flex shrink-0">
-                            <Image
+                            <img
                               src={category.icon}
                               alt=""
                               className="block"
